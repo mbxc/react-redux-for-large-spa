e8e38d4440ce6973352dfbb2fe4687db4f2ec2de
Mike He <i@bestmike007.com>
2017-06-29 11:31:52 +0800

    Upgrade deps & extract merge util for reducers.

--- webapp/reducers/home/index.js
+++ webapp/reducers/home/index.js
@@ -1,20 +1,13 @@
+import merge from 'reducers/utils/merge';
 import getIp from 'actions/home/getIp';
 import setName from 'actions/home/setName';
 
 export default {
   initialState: { name: null, ip: null, loading: false, error: false },
   reducers: {
-    [setName]: function(state, name) {
-      return Object.assign({}, state, {name});
-    },
-    [getIp]: function(state) {
-      return Object.assign({}, state, { loading: true, error: false, ip: null });
-    },
-    [getIp.success]: function(state, payload) {
-      return Object.assign({}, state, { loading: false, ip: payload.ip });
-    },
-    [getIp.error]: function(state, payload) {
-      return Object.assign({}, state, { loading: false, error: payload });
-    }
+    [setName]: merge(name => ({ name })),
+    [getIp]: merge(payload => ({ loading: true, error: false, ip: null })),
+    [getIp.success]: merge(payload => ({ loading: false, ip: payload.ip })),
+    [getIp.error]: merge(payload => ({ loading: false, error: payload }))
   }
 }
