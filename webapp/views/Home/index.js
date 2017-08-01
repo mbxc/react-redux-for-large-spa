e98424767187a38a5c6bd1f03ce7c804255123e4
Salieri <zyh825@hotmail.com>
2017-08-01 18:15:13 +0800

    fix: move action chaining demo to views/Welcome.jsx

--- webapp/views/Home/index.js
+++ webapp/views/Home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+
 // layouts
 import DefaultLayout from 'layouts/Default';
 import RightHeavyTwoColumnsLayout from 'layouts/RightHeavyTwoColumns';
+
 // page parts
 import Welcome from './Welcome';
 import LeftMenu from './LeftMenu';
@@ -10,11 +12,9 @@ import Footer from '../shared/Footer';
 
 // components
 import NameForm from 'components/NameForm';
+
 // actions
 import setName from 'actions/home/setName';
-
-import getIp from 'actions/home/getIp';
-import getAddressInfo from 'actions/home/getAddressInfo';
 
 function HomeMain({ name, ip, loading, error, addressInfo }) {
   if (loading) return <img src={IMG_LOADING} />;
@@ -22,7 +22,11 @@ function HomeMain({ name, ip, loading, error, addressInfo }) {
     <div>
       <Welcome {...{ name, ip, error }} />
       <div>
-        <NameForm onChange={v => setName(v)} value={name} label="What's your name please?" />
+        <NameForm
+          onChange={v => setName(v)}
+          value={name}
+          label="What's your name please?"
+        />
       </div>
       <h3>Your address Detail</h3>
       {JSON.stringify(addressInfo)}
@@ -30,33 +34,11 @@ function HomeMain({ name, ip, loading, error, addressInfo }) {
   );
 }
 
-class Home extends React.PureComponent {
-  componentDidMount() {
-    // an example of action chaining
-    Promise
-      .resolve()
-      // .then(openGlobalLoading)
-      .then(getIp)
-      .then(ipInfo => getAddressInfo(ipInfo.ip))
-      .then(() => {
-        // closeGlobalLoading();
-        console.log('good job!!');
-      })
-      .catch(() => {
-        // closeGlobalLoading();
-        console.log('oops, there was an error!');
-      });
-  }
-  render() {
-    const { home } = this.props;
-    return (
-      <RightHeavyTwoColumnsLayout
-        LeftMenu={<LeftMenu />}
-        Main={<HomeMain {...home} />}
-        minHeight={400}
-      />
-    );
-  }
-}
+const Home = ({ home }) =>
+  <RightHeavyTwoColumnsLayout
+    LeftMenu={<LeftMenu />}
+    Main={<HomeMain {...home} />}
+    minHeight={400}
+  />;
 
 export default Home;
